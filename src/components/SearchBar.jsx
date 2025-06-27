2f82ee0ddf193a633990395f1c1c0b73c9daab40
Shandy Ariady <shandylatoz851@gmail.com>
2025-06-27 17:44:07 +0700

    WIP: continue building make components

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useSearchParams } from 'react-router-dom';
+
+function SearchBar({ onSearch, placeholderText, className }) {
+  
+  const [searchParams, setSearchParams] = useSearchParams();
+  const initialKeyword = searchParams.get('judul') || '';
+  const [keyword, setKeyword] = useState(initialKeyword);
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    setSearchParams({ judul: keyword }); 
+    onSearch(keyword); 
+  };
+
+  useEffect(() => {
+    // Ambil keyword dari URL saat ini
+    const urlKeyword = searchParams.get('judul') || '';
+    if (urlKeyword !== keyword) {
+      setKeyword(urlKeyword);
+    }
+  }, [searchParams]);
+
+  // Menggabungkan class bawaan dengan class tambahan dari props
+  const containerClassName = `search-bar-container ${className || ''}`;
+
+  return (
+    <form className={containerClassName} onSubmit={handleSubmit}>
+      <input 
+        type="search" 
+        className="search-input" 
+        placeholder={placeholderText}
+        value={keyword}
+        onChange={(event) => setKeyword(event.target.value)}
+      />
+      <button type="submit" className="search-button">Cari</button>
+    </form>
+  );
+}
+
+SearchBar.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+  placeholderText: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+export default SearchBar;
